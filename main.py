e895587c70c403df9fde8b3c90d3b931f5f8c361
Dmitriy <45498288+hellog8888@users.noreply.github.com>
2025-04-13 17:43:09 +0300

    ver_0.7

--- main.py
+++ main.py
@@ -16,31 +16,37 @@ if __name__ == "__main__":
 
     print("-----------------------------------------------------")
     print(f"1.  IP:Location")
-    print(f"1.1 Чтение IP-адресов из локального файла hosts.txt")
+    print(f"1.1 Reading IP addresses from local hosts.txt file")
     ip_country_dict_lst = func.get_lst_from_file('hosts.txt')
     # Создание словаря IP:Location из локального файла hosts.txt
     ip_country_dict = func_ip_location.ip_and_country_lst_to_dict('ip_and_country_lst.txt')
-    print(f"1.2 Создание словаря IP:Location из локальных файлов hosts.txt + ip_and_country_lst.txt")
+    print(f"1.2 Creating an IP:Location dictionary from local hosts.txt + ip_and_country_lst.txt files")
     ip_country_dict = func_ip_location.create_ip_country_dict(ip_country_dict_lst, ip_country_dict)
-    print(f"Time execution #1: {time.time() - measurement_start_time:.4f} сек.")
+    print(f"Time execution #1: {time.time() - measurement_start_time:.4f} sec.")
 
     print("-----------------------------------------------------")
-    print(f"2.  Ping")
-    print(f"2.1 Поверка доступности. {count_ping_req} ICMP запросов на host")
+    print(f"2.  Ping test")
+    print(f"2.1 Availability check. {count_ping_req} ICMP requests to host")
     result_no_pass_ping = []
     result_pass_ping, result_pass_ping_no = func_ip_test.ping_cheking(ip_country_dict_lst, count_ping_req)
     print("")
-    print(f"2.2 IP не прошедших проверку: {0 if not result_pass_ping_no else [print(x) for x in result_no_pass_ping]}")
+    print(f"2.2 IP not verified: {0 if not result_pass_ping_no else [print(x) for x in result_no_pass_ping]}")
     print("")
-    print(f"2.3 TOP 3 IP успешных кандидата:")
-    [print(f"IP: {ip} - median delay {delay:.2f}") for ip, delay in list(result_pass_ping.items())[:3]]
+    print(f"2.3 TOP 3 IP of successful candidates:")
+    best_3_ip = list(result_pass_ping.items())[:3]
+    [print(f"IP: {ip} - median latency {delay:.2f}") for ip, delay in best_3_ip]
     print("")
-    print(f"Time execution #1 #2: {time.time() - measurement_start_time:.2f} сек.")
+    print(f"Time execution #1 #2: {time.time() - measurement_start_time:.2f} sec.")
 
-    # Stage 3
+    print("-----------------------------------------------------")
+    print(f"3.  Test Network Distance")
+    
+    print("2.3 TOP 3 IP of Network Distance:")
+    
 
     # Stage 4
 
+
     # Запись ip_country_dict в файл ip_and_country_lst
 
-    print(f"Total time execution: {time.time() - measurement_start_time:.4f} сек.")
+    print(f"Total time execution: {time.time() - measurement_start_time:.4f} sec.")
